1ea5ef1da23bb1b19e6e32e5b0d9e2073795ae75
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2022-06-19 22:44:07 -0300

    Added the Transform Interceptor

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalInterceptors(new TransformInterceptor())
+
+  await app.listen(3000)
 }
 bootstrap();
